8d679e5235cf8ce4b9d6fc3cc9ad5ca93b53b75e
护群神兽之点赞狂魔 <731014656@qq.com>
2016-12-28 08:57:28 +0800

    Update eqrcode.go

--- eqrcode.go
+++ eqrcode.go
@@ -43,7 +43,19 @@ func main() {
 	if os.Args[1] == "pic" {
 		model = true
 	}
-	fmt.Println("<html><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\"> <style>img{width:200px;height:200px;}div{float:left}</style></head>")
+	fmt.Println(`<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<style>
+img{width:200px;height:200px;}
+body{font-size:small;}
+div{  
+border-style: dashed;   
+display: inline-block;
+padding: 10px;  
+margin: 0px;   
+border-color: green;height:300px; 
+width:250px;    margin-top: 12px;   
+margin-bottom: 2.5px;}
+</style></head>`)
 	lines("./10003.csv", model)
 	fmt.Println("Done!")
 	defer fmt.Println("</html>")
